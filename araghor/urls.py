b2644f0b39e8bb29603794b880786a78f2124264
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2013-07-29 17:42:27 -0500

    Add a home page

--- araghor/urls.py
+++ araghor/urls.py
@@ -6,8 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'araghor.views.home', name='home'),
     # url(r'^araghor/', include('araghor.foo.urls')),
+    url(r'^$', 'araghor.views.home', name='home'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
