dce05fa402a8032e4105aecf0b357ede3bbb088d
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2013-08-11 00:41:58 -0500

    Add poll index pagination

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from polls.models import Poll, Choice
 from polls.forms import PollForm
 
 
 def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    context = {'latest_poll_list': latest_poll_list}
+    polls_per_page = 5
+    num_page_links = 5
+    poll_list = Poll.objects.order_by('-pub_date')
+    paginator = Paginator(poll_list, polls_per_page, orphans=1)
+
+    page = request.GET.get('page')
+    try:
+        polls = paginator.page(page)
+    except PageNotAnInteger:
+        polls = paginator.page(1)
+    except EmptyPage:
+        polls = paginator.page(paginator.num_pages)
+
+    total_page_list = [i for i in range(1, paginator.num_pages + 1)]
+    if polls.number <= num_page_links:
+        page_list = total_page_list[:num_page_links]
+    else:
+        page_list = total_page_list[(polls.number - num_page_links / 2):(polls.number + num_page_links / 2)]
+
+    context = {'polls': polls,
+               'page_list': page_list,
+              }
     return render(request, 'polls/index.html', context)
 
 
