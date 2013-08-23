d3b71815894c1d7bcc61e41ef6554df2c83dcac7
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2013-08-23 01:40:52 -0500

    Add search functionality to Blog app

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,22 @@ from django.shortcuts import render, get_object_or_404
 from models import Entry, Category
 
 def main(request):
-    entry_list = Entry.objects.all().order_by('-pub_date')
-    return render(request, 'blog/main.html', {'entry_list': entry_list})
+    entries_per_page = 5
+    num_page_links = 4
+    query = ""
+
+    if request.method == 'GET':
+        query = request.GET.get('query', '')
+
+    if query:
+        entry_list = Entry.objects.all().filter(title__icontains=query).order_by('-pub_date')
+    else:
+        entry_list = Entry.objects.all().order_by('-pub_date')[:5]
+
+    return render(request, 'blog/main.html',
+                  {'entry_list': entry_list,
+                   'query': query,
+                  })
 
 
 def detail(request, entry_id, **kwargs):
