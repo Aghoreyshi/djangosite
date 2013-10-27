5b648ace35699ab88c19908766dcb247de06f38f
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2013-10-27 01:19:32 -0700

    Embed CV using Google viewer so that navbar is in view

--- araghor/views.py
+++ araghor/views.py
@@ -9,8 +9,4 @@ def greyhome(request):
     return render(request, 'araghor/greyhome.html')
 
 def cv(request):
-    with open('araghor/static/arash_cv.pdf', 'r') as pdf:
-        response = HttpResponse(pdf.read(), mimetype='application/pdf')
-        response['Content-Disposition'] = 'inline;filename=arash_cv.pdf'
-        return response
-    pdf.closed
+    return render(request, 'araghor/cv.html')
