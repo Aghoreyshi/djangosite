2752ce055d5281e74e643ef9d30710c11c4c826b
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2013-10-30 00:45:12 -0500

    Remove padding between fields in poll creation form

--- polls/forms.py
+++ polls/forms.py
@@ -35,11 +35,11 @@ class PollForm(forms.Form):
     helper = FormHelper()
     helper.form_class = 'form-horizontal'
     helper.layout = Layout(
-        Field('question', css_class='input-lg form-control', placeholder='Enter Question', style='margin-bottom: 20px'),
-        Field('choice1', css_class='form-control', placeholder='Enter Choice', style='margin-bottom: 10px'),
-        Field('choice2', css_class='form-control', placeholder='Enter Choice', style='margin-bottom: 10px'),
-        Field('choice3', css_class='form-control', placeholder='Enter Choice', style='margin-bottom: 10px'),
-        Field('choice4', css_class='form-control', placeholder='Enter Choice', style='margin-bottom: 10px'),
+        Field('question', css_class='input-lg form-control', placeholder='Enter Question'),
+        Field('choice1', css_class='form-control', placeholder='Enter Choice'),
+        Field('choice2', css_class='form-control', placeholder='Enter Choice'),
+        Field('choice3', css_class='form-control', placeholder='Enter Choice'),
+        Field('choice4', css_class='form-control', placeholder='Enter Choice'),
         FormActions(
             Submit('save_changes', 'Create', css_class="btn-primary btn-lg", style="margin-top: 10px"),
         )
